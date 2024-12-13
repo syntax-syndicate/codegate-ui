dec754026870624708fa7828e870f23f826e7821
Luke Hinds <luke@stacklok.com>
2024-12-13 16:50:20 +0000

    Update src/components/Markdown.tsx
    Co-authored-by: Giuseppe Scuglia <peppescg@gmail.com>

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -131,7 +131,7 @@ export function Markdown({ children, fontSize, className = "" }: Props) {
         },
         a({ children, ...props }) {
           return (
-            <a className="text-blue-600 hover:text-blue-800 underline" {...props}>
+            <a className="text-blue-600 hover:text-blue-800 underline" target="_blank" {...props}>
               {children}
             </a>
           );
