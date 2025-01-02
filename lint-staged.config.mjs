f0782c082e3e577ec7bcf15970392984d1af5d7f
Giuseppe Scuglia <peppescg@gmail.com>
2025-01-02 10:52:03 +0100

    chore: add husky, lint-staged precommit

---
+++ lint-staged.config.mjs
@@ -0,0 +1,10 @@
+/**
+ * @type {import("lint-staged").Config}
+ */
+export default {
+  "**/*.{js,jsx,ts,tsx,mjs,cjs}": [
+    "npx prettier --write",
+    "npx eslint --fix",
+    "bash -c tsc -p ./tsconfig.app.json --noEmit",
+  ],
+};
