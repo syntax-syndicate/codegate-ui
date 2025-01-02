94fddb3875a547c6669c98d7bfa888da1263cd32
Giuseppe Scuglia <peppescg@gmail.com>
2025-01-02 16:01:24 +0100

    fix: replace copy to clipboard component

---
+++ src/components/CopyToClipboard.tsx
@@ -0,0 +1,33 @@
+import clsx from "clsx";
+import { ClipboardCopy } from "lucide-react";
+
+export function CopyToClipboard({
+  text,
+  className,
+}: {
+  className?: string;
+  text: string;
+}) {
+  const copyToClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(text);
+    } catch {
+      return;
+    }
+  };
+
+  return (
+    <button
+      onClick={copyToClipboard}
+      className={clsx(
+        `absolute top-4 right-8 p-2 rounded-md 
+    bg-gray-700/50 hover:bg-gray-700/70 
+    transition-opacity duration-200 
+    opacity-0 group-hover:opacity-100`,
+        className,
+      )}
+    >
+      <ClipboardCopy className="w-5 h-5 text-gray-200" />
+    </button>
+  );
+}
