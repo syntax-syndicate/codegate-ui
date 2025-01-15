749b83f365b0506386edcd006b49b8034299d076
Dan Barr <danbarr@users.noreply.github.com>
2025-01-15 10:41:52 -0500

    Update sidebar heading (#57)

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -20,7 +20,7 @@ export function Sidebar({
     return (
       <SidebarUI side="left" className="border-r-blue-200">
         <SidebarHeader className="bg-teal-25 py-6 pb-3 text-sm font-bold px-4">
-          History Prompts
+          Prompt History
         </SidebarHeader>
         <SidebarContent className="bg-teal-25">
           <SidebarMenu className="px-2">
@@ -39,7 +39,7 @@ export function Sidebar({
   return (
     <SidebarUI side="left" className="border-r-blue-200">
       <SidebarHeader className="bg-teal-25 py-6 pb-3 text-sm font-bold px-4">
-        History Prompts
+        Prompt History
       </SidebarHeader>
       <SidebarContent className="bg-teal-25">
         <SidebarGroup>{children}</SidebarGroup>
