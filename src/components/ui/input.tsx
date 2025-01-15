b58f93bbf6ea5cae373e0068c8a4f48cd7773435
Daniel Kantor <kd@stacklok.com>
2025-01-15 11:23:05 +0100

    more color fixes

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -12,7 +12,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <div
         className={cn(
           "flex h-10 items-center rounded-md border border-input bg-white pl-3 text-sm ring-offset-background focus-within:ring-1 focus-within:ring-ring focus-within:ring-offset-2",
-          className
+          className,
         )}
       >
         {icon}
@@ -20,11 +20,11 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           {...props}
           type="search"
           ref={ref}
-          className="w-full p-2 placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
+          className="w-full p-2 placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 bg-white"
         />
       </div>
     );
-  }
+  },
 );
 Input.displayName = "Input";
 
