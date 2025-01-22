0607e259a5d1c0092ddb27b006722c844751a9db
Daniel Kantor <kd@stacklok.com>
2025-01-22 13:42:12 +0100

    feat: use client-side routing for ui-kit

--- src/Page.tsx
+++ src/Page.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useHref, useNavigate } from "react-router-dom";
 
 import { RouteWorkspace } from "./routes/route-workspace";
 import { RouteWorkspaces } from "./routes/route-workspaces";
@@ -8,21 +8,26 @@ import { RouteChat } from "./routes/route-chat";
 import { RouteDashboard } from "./routes/route-dashboard";
 import { RouteCertificateSecurity } from "./routes/route-certificate-security";
 import { RouteWorkspaceCreation } from "./routes/route-workspace-creation";
+import { RouterProvider } from "@stacklok/ui-kit";
 
 export default function Page() {
+  const navigate = useNavigate();
+
   return (
-    <Routes>
-      <Route path="/" element={<RouteDashboard />} />
-      <Route path="/prompt/:id" element={<RouteChat />} />
-      <Route path="/help/:section" element={<RouteHelp />} />
-      <Route path="/certificates" element={<RouteCertificates />} />
-      <Route path="/workspace/:name" element={<RouteWorkspace />} />
-      <Route path="/workspaces" element={<RouteWorkspaces />} />
-      <Route path="/workspace/create" element={<RouteWorkspaceCreation />} />
-      <Route
-        path="/certificates/security"
-        element={<RouteCertificateSecurity />}
-      />
-    </Routes>
+    <RouterProvider navigate={navigate} useHref={useHref}>
+      <Routes>
+        <Route path="/" element={<RouteDashboard />} />
+        <Route path="/prompt/:id" element={<RouteChat />} />
+        <Route path="/help/:section" element={<RouteHelp />} />
+        <Route path="/certificates" element={<RouteCertificates />} />
+        <Route path="/workspace/:name" element={<RouteWorkspace />} />
+        <Route path="/workspaces" element={<RouteWorkspaces />} />
+        <Route path="/workspace/create" element={<RouteWorkspaceCreation />} />
+        <Route
+          path="/certificates/security"
+          element={<RouteCertificateSecurity />}
+        />
+      </Routes>
+    </RouterProvider>
   );
 }
